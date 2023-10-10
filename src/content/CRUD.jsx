9293841f3588a66849078c3d9e0ea71f7100eb5b
Max <maxnouts@gmail.com>
2023-10-10 09:24:33 +0200

    Updated CRUD data

--- src/content/CRUD.jsx
+++ src/content/CRUD.jsx
@@ -1,8 +1,25 @@
 function App() {
     return (
-      <>
+      <div className="contentContainer">
         <h1>CRUD eigen project</h1>
-      </>
+        <div className="inleidingContainer">
+          <h2>Inleiding</h2>
+          <p>
+            In dit project heb een eigen CRUD applicatie gemaakt. Dit heb ik gedaan in het framework React + Vite.
+          </p>
+        </div>
+        <div className="textContainer">
+          <h2>Onderzoek .env</h2>
+          <p>
+            Om meer te weten te komen over .env bestand in React + Vite heb ik een bieb <a href="https://i507258.hera.fhict.nl/semester3/CRUD/envFile.pdf">onderzoek</a> gedaan naar oplossing en het manier van toepassen van een .env bestand.
+            Het gebruik van .env bestand in React + Vite is iets anders dan een .env bestand React.
+          </p>
+          <h2>Process development</h2>
+          <p>
+            Om een duidelijk beeld te krijgen van mijn process heb ik een <a href="https://i507258.hera.fhict.nl/semester3/CRUD/Todolist.pdf">bestand</a> aan gemaakt waar ik mijn aanpassingingen bij hou.
+          </p>
+        </div>
+      </div>
     )
   }
 
