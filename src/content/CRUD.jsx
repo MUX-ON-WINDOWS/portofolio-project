bb9e2534d65fe9205a3e858d84c394caa60e04f3
Max <maxnouts@gmail.com>
2023-10-10 11:55:21 +0200

    Updated spaceweek

--- src/content/CRUD.jsx
+++ src/content/CRUD.jsx
@@ -12,7 +12,7 @@ function App() {
           <h2>Onderzoek .env</h2>
           <p>
             Om meer te weten te komen over .env bestand in React + Vite heb ik een bieb <a href="https://i507258.hera.fhict.nl/semester3/CRUD/envFile.pdf">onderzoek</a> gedaan naar oplossing en het manier van toepassen van een .env bestand.
-            Het gebruik van .env bestand in React + Vite is iets anders dan een .env bestand React.
+            Het gebruik van .env bestand in React + Vite is iets anders dan een .env bestand React. Bijvoorbeeld bij React is het process.env.SECRET_CODE en bij React + Vite is het import.meta.env.VITE_SECRET_CODE.
           </p>
           <h2>Process development</h2>
           <p>
