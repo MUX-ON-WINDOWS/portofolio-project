c22d932842ad4e387701c225baf58593a49b9b5d
Max <maxnouts@gmail.com>
2023-10-27 11:48:42 +0200

    Updated Carousel fixed view

--- src/components/Homewindow.jsx
+++ src/components/Homewindow.jsx
@@ -3,7 +3,8 @@ import '../Home.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
 // images import
 import spaceweekImage from '../img/Spaceweek.png';
 import AiHelpdeskImages from '../img/AiHelpdesk.png';
@@ -12,24 +13,26 @@ import { NavLink } from 'react-router-dom';
 
 function App() {
   const backgroundColor = { background: '#20176A' };
+  const TextColor = { color: 'white' };
   const data = [
-    { to: '/spaceweek', text: 'Out of space', url: spaceweekImage, color: { color: 'white' }, background: { background: backgroundColor } },
-    { to: '/CRUD', text: 'CRUD', url: CRUDImage, color: { color: 'white' }, background: { background: backgroundColor } },
-    { to: '/Discover', text: 'Discover', url: AiHelpdeskImages, color: { color: 'white' }, background: { background: backgroundColor } },
-    { to: '/Define', text: 'Define', url: AiHelpdeskImages, color: { color: 'white' }, background: { background: backgroundColor } },
-    { to: '/Develop', text: 'Develop', url: AiHelpdeskImages, color: { color: 'white' }, background: { background: backgroundColor } },
+    { to: '/spaceweek', text: 'Out of space', url: spaceweekImage, color: { color: TextColor }, background: { background: backgroundColor } },
+    { to: '/CRUD', text: 'CRUD', url: CRUDImage, color: { color: TextColor }, background: { background: backgroundColor } },
+    { to: '/Discover', text: 'Discover', url: AiHelpdeskImages, color: { color: TextColor }, background: { background: backgroundColor } },
+    { to: '/Define', text: 'Define', url: AiHelpdeskImages, color: { color: TextColor }, background: { background: backgroundColor } },
+    { to: '/Develop', text: 'Develop', url: AiHelpdeskImages, color: { color: TextColor }, background: { background: backgroundColor } },
   ];
 
   return (
     <div className='containerWindow'>
       <h1 className='name'>MAX ARNOUTS</h1>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
+        slidesPerView={4}
+        spaceBetween={90}
         pagination={{
-          clickable: true,
+          clickable: true
         }}
-        modules={[Pagination]}
+        navigation={true}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         {data.map((item, index) => (
